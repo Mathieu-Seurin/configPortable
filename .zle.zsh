6fd18c974823522fa28f24d23a74538e480d11ee
Mathieu-Seurin <matseurin@cegetel.net>
2016-05-02 12:00:35 +0200

    Update 02/05/2016 : Adding history search

--- .zle.zsh
+++ .zle.zsh
@@ -2,12 +2,13 @@ bindkey '^o' beginning-of-line
 bindkey '^v' end-of-line
 bindkey '^t' backward-char
 bindkey '^n' forward-char
+bindkey '^q' delete-char
 
 bindkey '\et' backward-word
 bindkey '\en' forward-word
-
-bindkey '^s' up-line-or-history
-bindkey '^r' down-line-or-history        
+        
+bindkey '^s' up-line-or-search
+bindkey '^r' down-line-or-search        
 
 function _kill-whole-line {
             zle beginning-of-line
